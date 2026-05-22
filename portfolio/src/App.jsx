8f863af6cd143227bd43628bc8bc22b2f380dff6
aldoesp <161883222+aldoesp@users.noreply.github.com>
2026-05-22 13:50:30 +0000

    Repair structure

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import SplashCursor from './components/SplashCursor'
+import SplashCursor from './components/effects/SplashCursor'
 
 function App() {
   const [count, setCount] = useState(0)
